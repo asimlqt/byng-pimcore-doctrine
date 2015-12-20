4a4e2275a60379a1f07c7f6c35a8e6b14cfd00d1
Asim Liaquat <asimlqt22@gmail.com>
2015-12-20 23:16:41 +0000

    .

--- cli-config.php
+++ cli-config.php
@@ -2,5 +2,10 @@
 
 require_once 'pimcore/cli/startup.php';
 
+// use Doctrine\Common\ClassLoader;
+// $classLoader = new ClassLoader('Doctrine\DBAL\Migrations', PIMCORE_WEBSITE_PATH . '/migrations');
+// $classLoader->register();
+
+
 $entityManager = \Byng\PimcoreDoctrine\Setup::getEntityManager();
 return \Doctrine\ORM\Tools\Console\ConsoleRunner::createHelperSet($entityManager);
